81a993eda9c5b3f097dc95ae0d00290e129cf81f
Mambo <kdevkr@gmail.com>
2022-03-06 10:16:27 +0900

    Init spring-boot

---
+++ src/test/java/com/example/springboot/ApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.springboot;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@Slf4j
+@SpringBootTest
+class ApplicationTests {
+    @Test
+    void contextLoads() {
+
+    }
+}
